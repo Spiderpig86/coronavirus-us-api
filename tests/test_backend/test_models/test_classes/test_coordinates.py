a2620856324bcf21f89ba6b7d3f6b4240b3ed7d2
Stanley Lim <slim679975@gmail.com>
2020-05-22 01:36:11 -0700

    :white_check_mark: test(test_classes): Added tests for Statistics and Coordinates classes

---
+++ tests/test_backend/test_models/test_classes/test_coordinates.py
@@ -0,0 +1,28 @@
+"""Coordinates Test
+
+Coordinates unit test.
+"""
+
+
+import pytest
+
+from backend.models.classes.coordinates import Coordinates
+
+@pytest.mark.parametrize(
+    "latitude, longitude",
+    [
+        (1, 2),
+        (-100, 1000000),
+        ("5", 6),
+        ("-41.123", "33.456")
+    ]
+)
+def test__coordinates__to_dict__success(latitude, longitude):
+    coordinates = Coordinates(latitude, longitude)
+
+    expected_response = {
+        "latitude": latitude,
+        "longitude": longitude
+    }
+
+    assert coordinates.to_dict() == expected_response
---
+++ tests/test_backend/test_models/test_classes/test_statistics.py
@@ -0,0 +1,26 @@
+"""Statistics Test
+
+Statistics unit test.
+"""
+
+
+import pytest
+
+from backend.models.classes.statistics import Statistics
+
+@pytest.mark.parametrize(
+    "confirmed, deaths",
+    [
+        (0, 0),
+        (80000, 2000),
+    ]
+)
+def test__statistics__to_dict__success(confirmed, deaths):
+    statistics = Statistics(confirmed, deaths)
+
+    expected_response = {
+        "confirmed": confirmed,
+        "deaths": deaths
+    }
+
+    assert statistics.to_dict() == expected_response

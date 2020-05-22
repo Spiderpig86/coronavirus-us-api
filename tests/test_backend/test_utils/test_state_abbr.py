44e6ec41931f81853808affcecf8f26175e6146c
Stanley Lim <slim679975@gmail.com>
2020-05-21 18:33:38 -0700

    :green_heart: chore(tests): Fix CI

--- tests/test_backend/test_utils/test_state_abbr.py
+++ tests/test_backend/test_utils/test_state_abbr.py
@@ -1,25 +1,16 @@
-
 import pytest
 
 from backend.utils.state_abbr import get_state_name
 
+
 @pytest.mark.parametrize(
     "code, expected_value",
-    [
-        ("NY", "new york"),
-        ("WA", "washington"),
-        ("ca", "california"),
-    ]
+    [("NY", "new york"), ("WA", "washington"), ("ca", "california"),],
 )
 def test__get_state_name__given_valid_parameter__success(code, expected_value):
     assert get_state_name(code) == expected_value
 
-@pytest.mark.parametrize(
-    "code",
-    [
-        (None),
-        ("XX")
-    ]
-)
+
+@pytest.mark.parametrize("code", [(None), ("XX")])
 def test__get_state_name__given_invalid_parameter__error(code):
     assert get_state_name(code) == "--"

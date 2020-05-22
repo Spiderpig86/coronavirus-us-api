482a758f57b58e46ba88f6e8eddc2d40df700109
Stanley Lim <slim679975@gmail.com>
2020-05-22 01:38:23 -0700

    :green_heart: chore: Fixing CI

--- tests/test_backend/test_models/test_classes/test_coordinates.py
+++ tests/test_backend/test_models/test_classes/test_coordinates.py
@@ -8,21 +8,13 @@ import pytest
 
 from backend.models.classes.coordinates import Coordinates
 
+
 @pytest.mark.parametrize(
-    "latitude, longitude",
-    [
-        (1, 2),
-        (-100, 1000000),
-        ("5", 6),
-        ("-41.123", "33.456")
-    ]
+    "latitude, longitude", [(1, 2), (-100, 1000000), ("5", 6), ("-41.123", "33.456")]
 )
 def test__coordinates__to_dict__success(latitude, longitude):
     coordinates = Coordinates(latitude, longitude)
 
-    expected_response = {
-        "latitude": latitude,
-        "longitude": longitude
-    }
+    expected_response = {"latitude": latitude, "longitude": longitude}
 
     assert coordinates.to_dict() == expected_response
--- tests/test_backend/test_models/test_classes/test_statistics.py
+++ tests/test_backend/test_models/test_classes/test_statistics.py
@@ -8,19 +8,11 @@ import pytest
 
 from backend.models.classes.statistics import Statistics
 
-@pytest.mark.parametrize(
-    "confirmed, deaths",
-    [
-        (0, 0),
-        (80000, 2000),
-    ]
-)
+
+@pytest.mark.parametrize("confirmed, deaths", [(0, 0), (80000, 2000),])
 def test__statistics__to_dict__success(confirmed, deaths):
     statistics = Statistics(confirmed, deaths)
 
-    expected_response = {
-        "confirmed": confirmed,
-        "deaths": deaths
-    }
+    expected_response = {"confirmed": confirmed, "deaths": deaths}
 
     assert statistics.to_dict() == expected_response

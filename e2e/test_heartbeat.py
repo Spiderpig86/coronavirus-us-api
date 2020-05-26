0b547ba1787ea4e8a4445e14d3529d0ecd1a777f
Stanley Lim <slim679975@gmail.com>
2020-05-25 18:18:15 -0700

    :green_heart: chore: Fix CI

--- e2e/test_heartbeat.py
+++ e2e/test_heartbeat.py
@@ -1,8 +1,10 @@
-from multiprocessing import Process
-import asynctest
 import asyncio
+from multiprocessing import Process
+
 import aiohttp
+import asynctest
 import uvicorn
+
 
 class E2ETestClient(asynctest.TestCase):
 
@@ -11,25 +13,23 @@ class E2ETestClient(asynctest.TestCase):
     LOG_LEVEL = "info"
 
     async def setUp(self):
-        self.process = Process(target=uvicorn.run,
-        args=("backend.main:api",),
-        kwargs={
-            "host": self.HOST,
-            "port": self.PORT,
-            "log_level": self.LOG_LEVEL
-        },
-        daemon=True)
+        self.process = Process(
+            target=uvicorn.run,
+            args=("backend.main:api",),
+            kwargs={"host": self.HOST, "port": self.PORT, "log_level": self.LOG_LEVEL},
+            daemon=True,
+        )
 
         self.process.start()
         await asyncio.sleep(0.5)
 
     async def tearDown(self):
-        self.process.terminate() 
+        self.process.terminate()
 
     async def test__heartbeat__success(self):
         async with aiohttp.ClientSession() as session:
-            async with session.get(f"http://127.0.0.1:{self.PORT}/api/health/heartbeat") as response:
+            async with session.get(
+                f"http://127.0.0.1:{self.PORT}/api/health/heartbeat"
+            ) as response:
                 data = await response.json()
-        assert data == {
-            "is_alive": True
-        }
+        assert data == {"is_alive": True}

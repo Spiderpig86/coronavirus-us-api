f90d2d30c1830dd89515401d764bb046180201ab
Stanley Lim <slim679975@gmail.com>
2020-06-18 01:21:53 -0700

    :green_heart: chore(containers.py): Fix CI

--- backend/utils/containers.py
+++ backend/utils/containers.py
@@ -1,11 +1,15 @@
-from dependency_injector import providers, containers
+from dependency_injector import containers, providers
 
-from backend.services.location_data_service import LocationDataService
 from backend.core.libs.cache import Cache
 from backend.services.data_source_service import DataSources
+from backend.services.location_data_service import LocationDataService
 
-class Container(containers.DeclarativeContainer): # TODO: Split into multiple containers?
+
+class Container(
+    containers.DeclarativeContainer
+):  # TODO: Split into multiple containers?
     cache = providers.Singleton(Cache)
+
 
 class DataSourceContainer(containers.DeclarativeContainer):
     data_sources = providers.Singleton(DataSources)
